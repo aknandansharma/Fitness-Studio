63acf7dbb7dc3fb3684e1a2e1b5365073bc85377
Aknandan Sharma <90132050+aknandansharma@users.noreply.github.com>
2023-08-22 15:08:29 +0530

    enroll now page complited

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import React from 'react'
-import EnrollNow from './components/Enroll Now/EnrollNow'
-import Discount from './components/Enroll Now/pages/Discount'
-import Register from './components/Enroll Now/pages/Register'
-import Login from './components/Enroll Now/pages/Login'
+import React from "react";
+import EnrollNow from "./components/Enroll Now/EnrollNow";
+import Discount from "./components/Enroll Now/pages/Discount";
+import Register from "./components/Enroll Now/pages/Register";
+import Login from "./components/Enroll Now/pages/Login";
+import UserDetails from './components/Enroll Now/pages/UserDetails'
 
 const App = () => {
-  return (
-    <>
-    {/* <Register/> */}
-    {/* <Login/> */}
-      {/* <EnrollNow/> */}
-      {/* <Discount/> */}
-    </>
-  )
-}
+    return (
+        <>
+            {/* <Register/> */}
+            {/* <Login/> */}
+            <EnrollNow/>
+            {/* <Discount/> */}
+            {/* <UserDetails/> */}
+        </>
+    );
+};
 
-export default App
+export default App;
