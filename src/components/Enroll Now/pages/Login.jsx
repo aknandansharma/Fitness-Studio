485bced90dcd58e0225e3279aee8e61440f8517e
Sandeep Sharma <SandeepSharma@gmail.com>
2026-03-26 18:39:41 +0530

    new code

--- src/components/Enroll Now/pages/Login.jsx
+++ src/components/Enroll Now/pages/Login.jsx
@@ -75,7 +75,7 @@ const Login = () => {
                             <label
                                 className='block mb-2 text-lg'
                                 htmlFor='email'>
-                                Email
+                                Email Sandeep
                             </label>
                             <input
                                 className='w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500 focus:ring-1 focus:ring-blue-500'
